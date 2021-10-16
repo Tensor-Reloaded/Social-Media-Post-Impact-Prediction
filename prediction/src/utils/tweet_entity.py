3562906116b54a357b82d06e1b35fc4560e2a878
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-10-16 21:48:49 +0300

    Added to Structure of the project

---
+++ prediction/src/utils/tweet_entity.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+import numpy as np
+
+
+@dataclass
+class Tweet:
+    tweet_id: int = None
+    tweet_text: str = None
+    tweet_image = None
+    user_id: int = None
+    user_followers: int = None
+    tweet_posted_date: str = None
