1ab653dbf216ed4d75b04ac3766d090a79b46a8a
Ioan <ioan.sava28@gmail.com>
2021-10-30 20:32:20 +0300

    setup for testing in React

--- social-media-post-impact-prediction-ui/src/components/App.tsx
+++ social-media-post-impact-prediction-ui/src/components/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HelloWorld from "./HelloWorld";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <HelloWorld />
     </div>
   );
 }
