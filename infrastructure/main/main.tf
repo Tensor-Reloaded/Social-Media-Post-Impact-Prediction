c94a511caff775b154fb8b74257b984535db43eb
csum112 <cataremix15@gmail.com>
2021-12-04 16:21:31 +0200

    Enabled pipeline & created IAM service policy

--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -8,19 +8,19 @@ terraform {
   }
   required_providers {
     mongodbatlas = {
-      source = "mongodb/mongodbatlas"
+      source  = "mongodb/mongodbatlas"
       version = "1.0.2"
     }
   }
 }
 
 locals {
-    services = toset([
-        "orchestration-service",
-        "account-management-service",
-        "ui-core-service"
-    ])
-    region = "eu-west-3"
+  services = toset([
+    "orchestration-service",
+    "account-management-service",
+    "ui-core-service"
+  ])
+  region = "eu-west-1"
 }
 
 module "networking" {
@@ -29,23 +29,23 @@ module "networking" {
 }
 
 module "ecr" {
-  source = "../modules/ecr"
+  source        = "../modules/ecr"
   service_names = local.services
 }
 
 module "ecs" {
-  source           = "../modules/ecs"
-  region 	   = local.region
-  vpc_id           = module.networking.vpc_id
-  public_subnet_ids= module.networking.public_subnet_ids
-  subnet_id        = module.networking.services_subnet_id
-  allowed_cidr     = module.networking.services_cidr
-  desired_capacity = 3
-  min_size         = 2
-  max_size         = 3
-  service_names    = local.services
-  repositories     = module.ecr.repositories
-  instance_type    = "t2.micro"
+  source            = "../modules/ecs"
+  region            = local.region
+  vpc_id            = module.networking.vpc_id
+  public_subnet_ids = module.networking.public_subnet_ids
+  subnet_id         = module.networking.services_subnet_id
+  allowed_cidr      = module.networking.services_cidr
+  desired_capacity  = 3
+  min_size          = 2
+  max_size          = 3
+  service_names     = local.services
+  repositories      = module.ecr.repositories
+  instance_type     = "t2.micro"
 }
 
 module "rds" {
