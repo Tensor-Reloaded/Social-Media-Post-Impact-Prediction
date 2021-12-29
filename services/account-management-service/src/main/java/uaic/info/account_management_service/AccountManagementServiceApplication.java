aa907261c768e74f2f33fdb7344000ecbc95425f
csum112 <cataremix15@gmail.com>
2021-12-29 19:38:53 +0200

    Changed EurekaClient annotation for DiscoveryClient

--- services/account-management-service/src/main/java/uaic/info/account_management_service/AccountManagementServiceApplication.java
+++ services/account-management-service/src/main/java/uaic/info/account_management_service/AccountManagementServiceApplication.java
@@ -2,9 +2,9 @@ package uaic.info.account_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountManagementServiceApplication {
 
--- services/orchestration-service/src/main/java/uaic/info/orchestrationservice/OrchestrationServiceApplication.java
+++ services/orchestration-service/src/main/java/uaic/info/orchestrationservice/OrchestrationServiceApplication.java
@@ -2,12 +2,12 @@ package uaic.info.orchestrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class OrchestrationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrchestrationServiceApplication.class, args);
