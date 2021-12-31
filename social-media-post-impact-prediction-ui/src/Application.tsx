e46d27c3aaf99373d72b4aac934d50d62c34d86a
Ciprian Danis <cdanis39@gmail.com>
2021-12-31 23:17:11 +0200

    Added login logic in UI + error page

--- social-media-post-impact-prediction-ui/src/Application.tsx
+++ social-media-post-impact-prediction-ui/src/Application.tsx
@@ -5,21 +5,25 @@ import Home from "./pages/Home";
 import GetPrediction from "./pages/GetPrediction";
 import PredictionsManagement from "./pages/PredictionsManagement";
 import CustomNavbar from "./components/CustomNavbar";
+import LoginError from "./pages/LoginError";
 import { getPredictions } from "./services/PredictionService";
 
 export default function Application() {
   return (
     <Router>
-      <CustomNavbar />
       <Switch>
-        <Route path="/" exact render={() => <Home />} />
-        <Route path="/get-prediction" render={() => <GetPrediction />} />
-        <Route
-          path="/predictions-management"
-          render={() => (
-            <PredictionsManagement predictions={getPredictions()} />
-          )}
+        <Route path="/login-error" render={() => <LoginError />} />
+        <>
+          <CustomNavbar />
+          <Route path="/" exact render={() => <Home />} />
+          <Route path="/get-prediction" render={() => <GetPrediction />} />
+          <Route
+            path="/predictions-management"
+            render={() => (
+              <PredictionsManagement predictions={getPredictions()} />
+            )}
         />
+        </>
       </Switch>
     </Router>
   );
--- social-media-post-impact-prediction-ui/src/services/TwitterService.tsx
+++ social-media-post-impact-prediction-ui/src/services/TwitterService.tsx
@@ -1,5 +1,56 @@
 import { Post } from "../models/Post";
+import axios from 'axios';
+import queryString from 'query-string';
+import { useEffect } from 'react';
 
-export function signIn(): void {}
+
+function getRedirectURL() : Promise<string | void> {
+    return axios({
+            url: `/api/v1/request_token`,
+            method: 'GET'
+        }).then(function(response) : string {
+            return response.data.redirectUrl;
+        }).catch(error => {
+            console.log(error);
+            window.location.href="/login-error?code=500";
+        });
+}
+
+function getAccessToken(requestToken, oauthVerifier) {
+    if (requestToken && oauthVerifier) {
+        axios({
+            url: `/api/v1/access_token`,
+            method: 'POST',
+            data: { requestToken, oauthVerifier }
+        }).then(response => {
+            localStorage.setItem('accessToken', response.data.accessToken);
+            window.location.href="/get-prediction";
+        }).catch(error => {
+            console.log(error);
+            window.location.href="/login-error?code=500";
+        });
+    }
+    else {
+        window.location.href="/login-error?code=Unauthorized App";
+    }
+}
+
+function retrieveTokensFromURL(responseURL) {
+    const {oauth_token, oauth_verifier} = queryString.parse(window.location.search);
+    return {oauth_token, oauth_verifier};
+}
+
+export function signIn(): void {
+    let redirectUrl = getRedirectURL();
+    const redirectUrlString = `${redirectUrl}`
+    window.location.href = redirectUrlString;
+
+    useEffect(() => {
+        (async() => {
+            const {oauth_token, oauth_verifier} = retrieveTokensFromURL(window.location.search);
+            getAccessToken(oauth_token, oauth_verifier);
+        })();
+    }, []);
+}
 
 export function postTweet(post: Post): void {}
